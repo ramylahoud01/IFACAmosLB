75ff1fbf734f0c648f7721daf057b81d62d81931
Rami Lahoud <rami.lahoud@aspiresoftware.com>
2024-10-26 11:03:07 +0300

    still testing

--- src/services/Get_News.js
+++ src/services/Get_News.js
@@ -1,8 +1,5 @@
 export const GetNews = async () => {
-  console.log(
-    "process.env.REACT_APP_API_BASE_URL",
-    process.env.REACT_APP_API_BASE_URL
-  );
+  console.log(`${process.env.REACT_APP_API_BASE_URL}/news`, "API");
   const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/news`, {
     headers: {
       "Content-Type": "application/json",
