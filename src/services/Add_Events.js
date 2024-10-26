60cf704999dd658a0b23997c36be22e372fd3bfa
Rami Lahoud <lahoud284@gmail.com>
2024-10-26 12:53:33 +0300

    test

--- src/services/Add_Events.js
+++ src/services/Add_Events.js
@@ -1,12 +1,10 @@
 export const AddEvents = async (jsonbody) => {
-    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/events`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(jsonbody),
-    });
-  
-    return response;
-  };
-  
+  return fetch("https://ifac-backend.vercel.app/events", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jsonbody),
+  });
+};
--- src/services/Add_News.js
+++ src/services/Add_News.js
@@ -4,7 +4,7 @@ export const AddNews = async (news, image) => {
   formData.append("content", news.content);
   formData.append("NewsImage", image); // Make sure to append the image file
 
-  const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/news`, {
+  const response = await fetch(`https://ifac-backend.vercel.app/news`, {
     method: "POST",
     body: formData, // No need to set Content-Type; fetch will handle this automatically for FormData
   });
